7c456e13e644cc234b965aa1986bc20b1d7abf41
unknown <nguyen8585@gmail.com>
2019-07-24 00:43:05 -0700

    Updates

---
+++ precache-manifest.fad99de6f5a2e266b0d2c6c815b432eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb57a24ca519d54ca29c46a768a6ea95",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1eda6c7e6e8f8efcb357",
+    "url": "/monsters-rolodex/static/css/main.996a5a09.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "1eda6c7e6e8f8efcb357",
+    "url": "/monsters-rolodex/static/js/main.b80b82b2.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
